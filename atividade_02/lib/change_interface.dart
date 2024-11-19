682534b1b47cbd44b5964713fcf25d192188e5ec
mutannejs <murillojustinoklein@gmail.com>
2024-11-19 12:09:07 -0300

    botões que alteram a interfaces concluidos

---
+++ atividade_02/lib/change_interface.dart
@@ -0,0 +1,170 @@
+import 'package:atividade_02/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<SharedPreferences> getSharedPreferences() => SharedPreferences.getInstance();
+
+class MenuAnchorColors extends ConsumerStatefulWidget {
+  const MenuAnchorColors({super.key});
+
+  @override
+  ConsumerState<MenuAnchorColors> createState() => _MenuAnchorColorsState();
+}
+
+class _MenuAnchorColorsState extends ConsumerState<MenuAnchorColors> {
+  final FocusNode _buttonFocusNode = FocusNode(debugLabel: 'Menu Button');
+
+  @override
+  void dispose() {
+    _buttonFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MenuAnchor(
+      childFocusNode: _buttonFocusNode,
+      menuChildren: [
+        MenuItemButton(
+          onPressed: () {
+            ref.read(colorThemeProvider.notifier).state = Colors.deepPurple;
+            getSharedPreferences().then((prefs) {prefs.setInt('colorThemeProvider', Colors.deepPurple.value);});
+          },
+          child: const Text('Roxo')
+        ),
+        MenuItemButton(
+          onPressed: () {
+            ref.read(colorThemeProvider.notifier).state = Colors.red;
+            getSharedPreferences().then((prefs) {prefs.setInt('colorThemeProvider', Colors.red.value);});
+          },
+          child: const Text('Vermelho')
+        ),
+        MenuItemButton(
+          onPressed: () {
+            ref.read(colorThemeProvider.notifier).state = Colors.green;
+            getSharedPreferences().then((prefs) {prefs.setInt('colorThemeProvider', Colors.green.value);});
+          },
+          child: const Text('Verde')
+        ),
+        MenuItemButton(
+          onPressed: () {
+            ref.read(colorThemeProvider.notifier).state = Colors.grey;
+            getSharedPreferences().then((prefs) {prefs.setInt('colorThemeProvider', Colors.grey.value);});
+          },
+          child: const Text('Azul')
+        )
+      ],
+      builder: (_, MenuController controller, Widget? child) {
+        return ElevatedButton(
+          focusNode: _buttonFocusNode,
+          onPressed: () {
+            if (controller.isOpen) {
+              controller.close();
+            } else {
+              controller.open();
+            }
+          },
+          child: const Text('Alterar cor'),
+        );
+      },
+    );
+  }
+}
+
+class MenuAnchorFonts extends ConsumerStatefulWidget {
+  const MenuAnchorFonts({super.key});
+
+  @override
+  ConsumerState<MenuAnchorFonts> createState() => _MenuAnchorFontsState();
+}
+
+class _MenuAnchorFontsState extends ConsumerState<MenuAnchorFonts> {
+  final FocusNode _buttonFocusNode = FocusNode(debugLabel: 'Menu Button');
+
+  @override
+  void dispose() {
+    _buttonFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MenuAnchor(
+      childFocusNode: _buttonFocusNode,
+      menuChildren: [
+        MenuItemButton(
+          onPressed: () {
+            ref.read(fontThemeProvider.notifier).state = 'Mono';
+            getSharedPreferences().then((prefs) {prefs.setString('fontThemeProvider', 'Mono');});
+          },
+          child: const Text('Mono', style: TextStyle(fontFamily: 'Mono'))
+        ),
+        MenuItemButton(
+          onPressed: () {
+            ref.read(fontThemeProvider.notifier).state = 'Arial';
+            getSharedPreferences().then((prefs) {prefs.setString('fontThemeProvider', 'Arial');});
+          },
+          child: const Text('Arial', style: TextStyle(fontFamily: 'Arial'))
+        ),
+        MenuItemButton(
+          onPressed: () {
+            ref.read(fontThemeProvider.notifier).state = 'Times';
+            getSharedPreferences().then((prefs) {prefs.setString('fontThemeProvider', 'Times');});
+          },
+          child: const Text('Times', style: TextStyle(fontFamily: 'Times'))
+        ),
+      ],
+      builder: (_, MenuController controller, Widget? child) {
+        return ElevatedButton(
+          focusNode: _buttonFocusNode,
+          onPressed: () {
+            if (controller.isOpen) {
+              controller.close();
+            } else {
+              controller.open();
+            }
+          },
+          child: const Text('Alterar fonte'),
+        );
+      },
+    );
+  }
+}
+
+class ChangeInterface extends ConsumerStatefulWidget {
+  const ChangeInterface({super.key});
+
+  @override
+  ConsumerState<ChangeInterface> createState() => _ChangeInterfaceState();
+}
+
+class _ChangeInterfaceState extends ConsumerState<ChangeInterface> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+      const MenuAnchorColors(),
+      const SizedBox(width: 20),
+      const MenuAnchorFonts(),
+      const SizedBox(width: 20),
+      IconButton.filledTonal(
+          onPressed: () {
+            var value = ref.read(fontSizeProvider.notifier).state += .1;
+            getSharedPreferences().then((onValue) {
+              onValue.setDouble('fontSizeProvider', value);
+            });
+          },
+          icon: const Icon(Icons.add_rounded)),
+      const SizedBox(width: 20),
+      IconButton.filledTonal(
+          onPressed: () async {
+            var value = ref.read(fontSizeProvider.notifier).state -= .1;
+            getSharedPreferences().then((prefs) {
+              prefs.setDouble('fontSizeProvider', value);
+            });
+          },
+          icon: const Icon(Icons.remove_rounded)),
+      const SizedBox(width: 20),
+    ]);
+  }
+}
--- atividade_02/lib/main.dart
+++ atividade_02/lib/main.dart
@@ -1,18 +1,33 @@
 import 'package:atividade_02/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+var colorThemeProvider;
+var fontThemeProvider;
+var fontSizeProvider;
+
+void main() async {
+  int? ctp;
+  String? ftp;
+  double? fsp;
+
+  SharedPreferences
+    .getInstance()
+    .then((prefs) {
+      // return prefs.clear();
+      ctp = prefs.getInt('colorThemeProvider');
+      ftp = prefs.getString('fontThemeProvider');
+      fsp = prefs.getDouble('fontSizeProvider');
+    });
+  // return;
+
+  colorThemeProvider = StateProvider((ref) => Color(ctp ?? Colors.deepPurple.value));
+  fontThemeProvider = StateProvider((ref) => ftp ?? 'Arial');
+  fontSizeProvider = StateProvider((ref) => fsp ?? 1.0);
+
   runApp(const ProviderScope(child: MyApp()));
 }
-
-Future<SharedPreferences> getSharedPreferences() => SharedPreferences.getInstance();
-
-var colorThemeProvider = StateProvider((ref) => Colors.deepPurple);
-var fontThemeProvider = StateProvider((ref) => GoogleFonts.getFont('Lato').fontFamily);
-var fontSizeProvider = StateProvider((ref) => 15.0);
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
@@ -20,19 +35,15 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
 
-    SharedPreferences
-      .getInstance()
-      .then((prefs) {
-        ref.read(fontSizeProvider.notifier).state = prefs.getDouble('fontSizeProvider') ?? 15.0;
-        ref.read(fontThemeProvider.notifier).state = prefs.getString('fontThemeProvider') ?? ref.watch(fontThemeProvider);
-      });
-
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: ref.watch(colorThemeProvider)),
-        fontFamily: ref.watch(fontThemeProvider),
+        textTheme: Theme.of(context).textTheme.apply(
+          fontFamily: ref.watch(fontThemeProvider),
+          fontSizeFactor: ref.watch(fontSizeProvider),
+        )
       ),
       routes: {
         '/': (_) => const HomePage(),
